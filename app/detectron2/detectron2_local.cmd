8941f3c5b441ee59506e6c2ae5d8a10113c7b6b5
Yuya KITANO <uyaspecial@gmail.com>
2024-07-06 17:34:39 +0900

    Initialize

--- app/detectron2/detectron2_local.cmd
+++ app/detectron2/detectron2_local.cmd
@@ -10,20 +10,20 @@ REM ----------------------------------------------------------------------------
 REM # Output results by Detectron2
 
 REM https://github.com/facebookresearch/detectron2/blob/main/MODEL_ZOO.md#coco-instance-segmentation-baselines-with-mask-r-cnn
-C:/home/python3111x64/python.exe  detectron2.py --config-file ./configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml  --input %~n1.png  --output %~n1_InstanceSegmentation.png  --opts MODEL.DEVICE cpu MODEL.WEIGHTS detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl
-REM C:/home/python3111x64/python.exe  detectron2.py --config-file ./configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml  --input %~n1.png  --output %~n1_InstanceSegmentation.png  --opts MODEL.DEVICE cpu MODEL.WEIGHTS ./pre-models/model_final_f10217.pkl
+REM C:/home/python3111x64/python.exe  detectron2.py --config-file ./configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml  --input %~n1.png  --output %~n1_InstanceSegmentation.png  --opts MODEL.DEVICE cpu MODEL.WEIGHTS detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl
+C:/home/python3111x64/python.exe  detectron2.py --config-file ./configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml  --input %~n1.png  --output %~n1_InstanceSegmentation.png  --opts MODEL.DEVICE cpu MODEL.WEIGHTS ./pre-models/model_final_f10217.pkl
 copy  %~n1_InstanceSegmentation.png  %~n1_Detectron2
 
-C:/home/python3111x64/python.exe  detectron2.py --config-file ./configs/COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x.yaml        --input %~n1.png  --output %~n1_Keypoints.png             --opts MODEL.DEVICE cpu MODEL.WEIGHTS detectron2://COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x/138363331/model_final_997cc7.pkl
-REM C:/home/python3111x64/python.exe  detectron2.py --config-file ./configs/COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x.yaml        --input %~n1.png  --output %~n1_Keypoints.png             --opts MODEL.DEVICE cpu MODEL.WEIGHTS ./pre-models/model_final_997cc7.pkl
+REM C:/home/python3111x64/python.exe  detectron2.py --config-file ./configs/COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x.yaml        --input %~n1.png  --output %~n1_Keypoints.png             --opts MODEL.DEVICE cpu MODEL.WEIGHTS detectron2://COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x/138363331/model_final_997cc7.pkl
+C:/home/python3111x64/python.exe  detectron2.py --config-file ./configs/COCO-Keypoints/keypoint_rcnn_R_101_FPN_3x.yaml        --input %~n1.png  --output %~n1_Keypoints.png             --opts MODEL.DEVICE cpu MODEL.WEIGHTS ./pre-models/model_final_997cc7.pkl
 copy  %~n1_Keypoints.png             %~n1_Detectron2
 
-C:/home/python3111x64/python.exe  detectron2.py --config-file ./configs/COCO-PanopticSegmentation/panoptic_fpn_R_50_3x.yaml   --input %~n1.png  --output %~n1_PanopticSegmentation.png  --opts MODEL.DEVICE cpu MODEL.WEIGHTS detectron2://COCO-PanopticSegmentation/panoptic_fpn_R_50_3x/139514569/model_final_c10459.pkl
-REM C:/home/python3111x64/python.exe  detectron2.py --config-file ./configs/COCO-PanopticSegmentation/panoptic_fpn_R_50_3x.yaml   --input %~n1.png  --output %~n1_PanopticSegmentation.png  --opts MODEL.DEVICE cpu MODEL.WEIGHTS ./pre-models/model_final_c10459.pkl
+REM C:/home/python3111x64/python.exe  detectron2.py --config-file ./configs/COCO-PanopticSegmentation/panoptic_fpn_R_50_3x.yaml   --input %~n1.png  --output %~n1_PanopticSegmentation.png  --opts MODEL.DEVICE cpu MODEL.WEIGHTS detectron2://COCO-PanopticSegmentation/panoptic_fpn_R_50_3x/139514569/model_final_c10459.pkl
+C:/home/python3111x64/python.exe  detectron2.py --config-file ./configs/COCO-PanopticSegmentation/panoptic_fpn_R_50_3x.yaml   --input %~n1.png  --output %~n1_PanopticSegmentation.png  --opts MODEL.DEVICE cpu MODEL.WEIGHTS ./pre-models/model_final_c10459.pkl
 copy  %~n1_PanopticSegmentation.png  %~n1_Detectron2
 
-C:/home/python3111x64/python.exe  detectron2.py --config-file ./configs/COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml           --input %~n1.png  --output %~n1_Detection.png             --opts MODEL.DEVICE cpu MODEL.WEIGHTS detectron2://COCO-Detection/faster_rcnn_R_50_FPN_3x/137849458/model_final_280758.pkl
-REM C:/home/python3111x64/python.exe  detectron2.py --config-file ./configs/COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml           --input %~n1.png  --output %~n1_Detection.png             --opts MODEL.DEVICE cpu MODEL.WEIGHTS ./pre-models/model_final_280758.pkl
+REM C:/home/python3111x64/python.exe  detectron2.py --config-file ./configs/COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml           --input %~n1.png  --output %~n1_Detection.png             --opts MODEL.DEVICE cpu MODEL.WEIGHTS detectron2://COCO-Detection/faster_rcnn_R_50_FPN_3x/137849458/model_final_280758.pkl
+C:/home/python3111x64/python.exe  detectron2.py --config-file ./configs/COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml           --input %~n1.png  --output %~n1_Detection.png             --opts MODEL.DEVICE cpu MODEL.WEIGHTS ./pre-models/model_final_280758.pkl
 copy  %~n1_Detection.png             %~n1_Detectron2
 REM --------------------------------------------------------------------------------
 REM # Zip results
